272743efa8db0e51fdb8ab33858b1ec7b6e0e310
Ansh Sharma <wiredfoxterrier17@gmail.com>
2020-12-08 04:18:22 +0530

    Addition of main class for calculator

---
+++ Calculator /README.md
@@ -0,0 +1,9 @@
+# TeamProjects
+
+## README File for the calculator project 
+
+## People Contributing are 
+##### add your name when you contribute to this repo.
+- Ansh Sharma 
+
+
---
+++ Calculator /calculator.py
@@ -0,0 +1,16 @@
+class Calculator():
+"""
+docstring for Calculator
+
+Description:
+This is the main class.
+
+
+
+Record:
+|Sr.No.| Name | Date | Changes made |
+|1.|Ansh Sharma|8th December,2020|Creation of the class|
+"""
+	def __init__(self, arg):
+		super(Calculator, self).__init__()
+		self.arg = arg
